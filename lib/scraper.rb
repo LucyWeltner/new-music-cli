64fa8c870c77ffe2b88f739d7a1405564d7ae6a8
Lucy Weltner <lucyiweltner@gmail.com>
2019-12-02 19:23:07 +0000

    added scrape from youtube method to scrqaper clas

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,4 +3,9 @@ require "nokogiri"
 
 class Scraper
   self.scrape_from_youtube(link)
+    html = Nokogiri::HTML(open("https://www.youtube.com/watch?v=rLCwc_nrimk&list=PLP4CSgl7K7or84AAhr7zlLNpghEnKWu2c&index=1"))
+    p html.css("#description").text
+  end
+end
     
+Scraper.scrape_from_youtube("https://www.youtube.com/watch?v=rLCwc_nrimk&list=PLP4CSgl7K7or84AAhr7zlLNpghEnKWu2c&index=1")
