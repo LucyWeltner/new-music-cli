0db65ee2c5b7066ab95f9790de4e6b3b3e297596
Lucy Weltner <lucyiweltner@gmail.com>
2019-12-03 18:48:28 +0000

    streamlined code and deleted unnecessary class variable

--- lib/songs.rb
+++ lib/songs.rb
@@ -1,7 +1,6 @@
 require_relative "../lib/scraper.rb"
 class Song
   attr_accessor :title, :artist, :url
-  @@description = []
   @@songs = []
   def initialize(title, artist, url)
     @title = title 
@@ -15,19 +14,18 @@ class Song
     first_songs_index = description_array.find_index{|line| line.include?("- ")}
     #to get the index of the last song, reverse the array, find the index of the first song, then multiply that number by -1 (because it's the same distance from the beginning of the reversed array as it is from the end of the original array, ie the second element of the reversed array is the second to last element of the original array)
     last_songs_index = description_array.reverse.find_index{|line| line.include?(" - ")}*-1
-    #select all the content between the first song and the last song. Put that content into the description class variable.
-    @@description << description_array[first_songs_index..last_songs_index]
-    @@description = @@description.flatten
+    #select and return all the content between the first song and the last song. 
+    description_array = description_array[first_songs_index..last_songs_index]
   end
     
   def self.make_songs_from_description
     #make an array of songs. Each song is represented by a smaller array with title and artist elements.
-    self.trim_description
-    song_array = @@description.select {|line| line.include?(" - ")}
+    description = self.trim_description
+    song_array = description.select {|line| line.include?(" - ")}
     song_aoa = song_array.map{|title_and_artist| title_and_artist.split(" - ")}
     
     #make an array of urls. Each song should have a matching URL.
-    url_array = @@description.select{|line| line.include?("https://")}
+    url_array = description.select{|line| line.include?("https://")}
     
     #iterate over song array and create a new song object for each element of the array. 
     song_aoa.each_with_index do |song, index|
@@ -37,11 +35,11 @@ class Song
   end
   
   def self.search_by_title(a_title)
-    @@songs.find{|song| song.title == a_title}
+    self.all.find{|song| song.title == a_title}
   end 
   
   def self.search_by_artist(an_artist)
-    found = @@songs.select{|song| song.artist == an_artist
+    found = self.all.select{|song| song.artist == an_artist}
     #If you only find one song by an artist, return only that song
     if found.length == 1 
       found.length[0]
@@ -49,14 +47,17 @@ class Song
   end 
   
   def self.search 
-    puts "You can search for a song by typing the title or artist."
+    puts "Search for a song by typing the title or artist."
     input = gets.chomp!
     if search_by_artist(input.capitalize)
       results = search_by_artist(input.capitalize)
+      #if there are multiple results, iterate through an array to show each one
       if results.class == Array 
         puts "There are #{results.length} songs that match your query:"
         results.each_with_index do |song, index|
           puts "#{index+1}. #{song.title} by #{song.artist}. Listen at #{song.url}"
+        end
+      #if there is only one result, show only that result
       else 
         puts "The song that matches your query is #{results.title} by #{results.artist} which you can listen to at #{results.url}."
       end 
@@ -68,11 +69,15 @@ class Song
     end 
   end 
   
-  def self.all_songs 
-    @@songs 
+  def self.all 
+    @@songs
   end 
+  
+  def self.display_all
+    self.all_songs.each_with_index
+  end
 end 
 
 Song.make_songs_from_description
-Song.all_songs
+p Song.search_by_title("It Just Doesn't Happen")
 
